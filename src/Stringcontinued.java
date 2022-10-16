02e2096190d1e0ecf56891a9929ca3a6b1463c15
Ryan Lin <Ryanl68@nycstudents.net>
2022-10-16 09:55:40 -0400

    Initial commit

---
+++ src/Stringcontinued.java
@@ -0,0 +1,14 @@
+public class Stringcontinued {
+    public static void main(String[] args){
+        String greeting = "Hello, friend!\n";
+        System.out.print(greeting);
+        System.out.print("Nice to see you!\n");
+        System.out.println("Goodbye!");
+
+
+        System.out.println("Nice to see you!");
+        System.out.println("Goodbye!");
+
+
+    }
+}
